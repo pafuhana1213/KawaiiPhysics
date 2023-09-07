21098504b4fcb1e29f4454af4a3ef389c89c9cba
pafuhana@hotmail.co.jp <PafuhanaOkada1236>
2023-09-07 22:36:15 +0900

    Add KawaiiPhysicsLibrary for AnimNode Functions

---
+++ Plugins/KawaiiPhysics/Source/KawaiiPhysics/Private/KawaiiPhysicsLibrary.cpp
@@ -0,0 +1,203 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KawaiiPhysicsLibrary.h"
+
+#include "AnimNode_KawaiiPhysics.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogKawaiiPhysicsLibrary, Verbose, All);
+
+FKawaiiPhysicsReference UKawaiiPhysicsLibrary::ConvertToKawaiiPhysics(const FAnimNodeReference& Node,
+	EAnimNodeReferenceConversionResult& Result)
+{
+	return FAnimNodeReference::ConvertToType<FKawaiiPhysicsReference>(Node, Result);
+}
+
+FKawaiiPhysicsReference UKawaiiPhysicsLibrary::SetPhysicsSettings(const FKawaiiPhysicsReference& KawaiiPhysics,
+	FKawaiiPhysicsSettings PhysicsSettings)
+{
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+		TEXT("SetPhysicsSettings"),
+		[PhysicsSettings](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+		{
+			InKawaiiPhysics.PhysicsSettings = PhysicsSettings;
+		});
+
+	return KawaiiPhysics;
+}
+
+FKawaiiPhysicsSettings UKawaiiPhysicsLibrary::GetPhysicsSettings(const FKawaiiPhysicsReference& KawaiiPhysics)
+{
+	FKawaiiPhysicsSettings PhysicsSettings;
+	
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+	TEXT("GetPhysicsSettings"),
+	[&PhysicsSettings](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+	{
+		PhysicsSettings = InKawaiiPhysics.PhysicsSettings;
+	});
+
+	return PhysicsSettings;
+}
+
+FKawaiiPhysicsReference UKawaiiPhysicsLibrary::SetTeleportDistanceThreshold(
+	const FKawaiiPhysicsReference& KawaiiPhysics, float TeleportDistanceThreshold)
+{
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+		TEXT("SetTeleportDistanceThreshold"),
+		[TeleportDistanceThreshold](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+		{
+			InKawaiiPhysics.TeleportDistanceThreshold = TeleportDistanceThreshold;
+		});
+
+	return KawaiiPhysics;
+}
+
+float UKawaiiPhysicsLibrary::GetTeleportDistanceThreshold(const FKawaiiPhysicsReference& KawaiiPhysics)
+{
+	float TeleportDistanceThreshold;
+	
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+	TEXT("GetTeleportDistanceThreshold"),
+	[&TeleportDistanceThreshold](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+	{
+		TeleportDistanceThreshold = InKawaiiPhysics.TeleportDistanceThreshold;
+	});
+
+	return TeleportDistanceThreshold;
+}
+
+FKawaiiPhysicsReference UKawaiiPhysicsLibrary::SetTeleportRotationThreshold(
+	const FKawaiiPhysicsReference& KawaiiPhysics, float TeleportRotationThreshold)
+{
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+		TEXT("SetTeleportRotationThreshold"),
+		[TeleportRotationThreshold](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+		{
+			InKawaiiPhysics.TeleportRotationThreshold = TeleportRotationThreshold;
+		});
+
+	return KawaiiPhysics;
+}
+
+float UKawaiiPhysicsLibrary::GetTeleportRotationThreshold(const FKawaiiPhysicsReference& KawaiiPhysics)
+{
+	float TeleportRotationThreshold;
+	
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+	TEXT("GetTeleportRotationThreshold"),
+	[&TeleportRotationThreshold](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+	{
+		TeleportRotationThreshold = InKawaiiPhysics.TeleportRotationThreshold;
+	});
+
+	return TeleportRotationThreshold;
+}
+
+FKawaiiPhysicsReference UKawaiiPhysicsLibrary::SetGravity(
+	const FKawaiiPhysicsReference& KawaiiPhysics, FVector Gravity)
+{
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+		TEXT("SetGravity"),
+		[Gravity](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+		{
+			InKawaiiPhysics.Gravity = Gravity;
+		});
+
+	return KawaiiPhysics;
+}
+
+FVector UKawaiiPhysicsLibrary::GetGravity(const FKawaiiPhysicsReference& KawaiiPhysics)
+{
+	FVector Gravity;
+	
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+	TEXT("GetGravity"),
+	[&Gravity](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+	{
+		Gravity = InKawaiiPhysics.Gravity;
+	});
+
+	return Gravity;
+}
+
+FKawaiiPhysicsReference UKawaiiPhysicsLibrary::SetEnableWind(
+	const FKawaiiPhysicsReference& KawaiiPhysics, bool EnableWind)
+{
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+		TEXT("SetEnableWind"),
+		[EnableWind](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+		{
+			InKawaiiPhysics.bEnableWind = EnableWind;
+		});
+
+	return KawaiiPhysics;
+}
+
+bool UKawaiiPhysicsLibrary::GetEnableWind(const FKawaiiPhysicsReference& KawaiiPhysics)
+{
+	bool EnableWind;
+	
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+	TEXT("GetEnableWind"),
+	[&EnableWind](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+	{
+		EnableWind = InKawaiiPhysics.bEnableWind;
+	});
+
+	return EnableWind;
+}
+
+FKawaiiPhysicsReference UKawaiiPhysicsLibrary::SetWindScale(
+	const FKawaiiPhysicsReference& KawaiiPhysics, float WindScale)
+{
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+		TEXT("SetWindScale"),
+		[WindScale](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+		{
+			InKawaiiPhysics.WindScale = WindScale;
+		});
+
+	return KawaiiPhysics;
+}
+
+float UKawaiiPhysicsLibrary::GetWindScale(const FKawaiiPhysicsReference& KawaiiPhysics)
+{
+	float WindScale;
+	
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+	TEXT("GetWindScale"),
+	[&WindScale](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+	{
+		WindScale = InKawaiiPhysics.WindScale;
+	});
+
+	return WindScale;
+}
+
+FKawaiiPhysicsReference UKawaiiPhysicsLibrary::SetAllowWorldCollision(
+	const FKawaiiPhysicsReference& KawaiiPhysics, bool AllowWorldCollision)
+{
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+		TEXT("SetAllowWorldCollision"),
+		[AllowWorldCollision](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+		{
+			InKawaiiPhysics.bAllowWorldCollision = AllowWorldCollision;
+		});
+
+	return KawaiiPhysics;
+}
+
+bool UKawaiiPhysicsLibrary::GetAllowWorldCollision(const FKawaiiPhysicsReference& KawaiiPhysics)
+{
+	bool AllowWorldCollision;
+	
+	KawaiiPhysics.CallAnimNodeFunction<FAnimNode_KawaiiPhysics>(
+	TEXT("GetAllowWorldCollision"),
+	[&AllowWorldCollision](FAnimNode_KawaiiPhysics& InKawaiiPhysics)
+	{
+		AllowWorldCollision = InKawaiiPhysics.bAllowWorldCollision;
+	});
+
+	return AllowWorldCollision;
+}
---
+++ Plugins/KawaiiPhysics/Source/KawaiiPhysics/Public/KawaiiPhysicsLibrary.h
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNodeReference.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "KawaiiPhysicsLibrary.generated.h"
+
+struct FAnimNode_KawaiiPhysics;
+
+USTRUCT(BlueprintType)
+struct FKawaiiPhysicsReference : public FAnimNodeReference
+{
+	GENERATED_BODY()
+
+	typedef FAnimNode_KawaiiPhysics FInternalNodeType;
+};
+
+/**
+ * Exposes operations to be performed on a blend space anim node.
+ */
+UCLASS()
+class KAWAIIPHYSICS_API UKawaiiPhysicsLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	/** Get a KawaiiPhysics from an anim node */
+	UFUNCTION(BlueprintCallable, Category = "Kawaii Physics", meta = (BlueprintThreadSafe, ExpandEnumAsExecs = "Result"))
+	static FKawaiiPhysicsReference ConvertToKawaiiPhysics(const FAnimNodeReference& Node, EAnimNodeReferenceConversionResult& Result);
+
+	/** Get a KawaiiPhysics from an anim node (pure). */
+	UFUNCTION(BlueprintPure, Category = "Kawaii Physics", meta = (BlueprintThreadSafe, DisplayName = "Convert to Kawaii Physics (Pure)"))
+	static void ConvertToKawaiiPhysicsPure(const FAnimNodeReference& Node, FKawaiiPhysicsReference& KawaiiPhysics, bool& Result)
+	{
+		EAnimNodeReferenceConversionResult ConversionResult;
+		KawaiiPhysics = ConvertToKawaiiPhysics(Node, ConversionResult);
+		Result = (ConversionResult == EAnimNodeReferenceConversionResult::Succeeded);
+	}
+
+	/** Set PhysicsSettings */
+	UFUNCTION(BlueprintCallable, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	static FKawaiiPhysicsReference SetPhysicsSettings(const FKawaiiPhysicsReference& KawaiiPhysics, FKawaiiPhysicsSettings PhysicsSettings);
+	/** Get PhysicsSettings */
+	UFUNCTION(BlueprintPure, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	static FKawaiiPhysicsSettings GetPhysicsSettings(const FKawaiiPhysicsReference& KawaiiPhysics);
+
+	/** Set TeleportDistanceThreshold */
+	UFUNCTION(BlueprintCallable, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	static FKawaiiPhysicsReference SetTeleportDistanceThreshold(const FKawaiiPhysicsReference& KawaiiPhysics, float TeleportDistanceThreshold);
+	/** Get TeleportDistanceThreshold */
+	UFUNCTION(BlueprintPure, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	static float GetTeleportDistanceThreshold(const FKawaiiPhysicsReference& KawaiiPhysics);
+
+	/** Set TeleportRotationThreshold */
+	UFUNCTION(BlueprintCallable, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	FKawaiiPhysicsReference SetTeleportRotationThreshold(const FKawaiiPhysicsReference& KawaiiPhysics,float TeleportRotationThreshold);
+	/** Get TeleportRotationThreshold */
+	UFUNCTION(BlueprintPure, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	float GetTeleportRotationThreshold(const FKawaiiPhysicsReference& KawaiiPhysics);
+
+	/** Set Gravity */
+	UFUNCTION(BlueprintCallable, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	FKawaiiPhysicsReference SetGravity(const FKawaiiPhysicsReference& KawaiiPhysics, FVector Gravity);
+	/** Get Gravity */
+	UFUNCTION(BlueprintPure, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	FVector GetGravity(const FKawaiiPhysicsReference& KawaiiPhysics);
+
+	/** Set EnableWind */
+	UFUNCTION(BlueprintCallable, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	FKawaiiPhysicsReference SetEnableWind(const FKawaiiPhysicsReference& KawaiiPhysics, bool EnableWind);
+	/** Get EnableWind */
+	UFUNCTION(BlueprintPure, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	bool GetEnableWind(const FKawaiiPhysicsReference& KawaiiPhysics);
+
+	/** Set WindScale */
+	UFUNCTION(BlueprintCallable, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	FKawaiiPhysicsReference SetWindScale(const FKawaiiPhysicsReference& KawaiiPhysics, float WindScale);
+	/** Get WindScale */
+	UFUNCTION(BlueprintPure, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	float GetWindScale(const FKawaiiPhysicsReference& KawaiiPhysics);
+
+	/** Set AllowWorldCollision */
+	UFUNCTION(BlueprintCallable, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	FKawaiiPhysicsReference SetAllowWorldCollision(const FKawaiiPhysicsReference& KawaiiPhysics,bool AllowWorldCollision);
+	/** Get AllowWorldCollision */
+	UFUNCTION(BlueprintPure, Category = "Kawaii Physics", meta=(BlueprintThreadSafe))
+	bool GetAllowWorldCollision(const FKawaiiPhysicsReference& KawaiiPhysics);
+};
