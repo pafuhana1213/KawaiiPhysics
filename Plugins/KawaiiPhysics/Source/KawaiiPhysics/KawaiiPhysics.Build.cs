a109c21ffb41dbcf6cef6fb7158c356c0abf4703
pafuhana1213 <pafuhana@hotmail.co.jp>
2025-06-13 21:07:20 +0900

    [Fix] Refactored dependency modules to fix warnings/errors related to the StructUtils for Unreal Engine 5.5 and later.

--- Plugins/KawaiiPhysics/Source/KawaiiPhysics/KawaiiPhysics.Build.cs
+++ Plugins/KawaiiPhysics/Source/KawaiiPhysics/KawaiiPhysics.Build.cs
@@ -8,32 +8,15 @@ public class KawaiiPhysics : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicIncludePaths.AddRange(
-			new string[]
-			{
-				// ... add public include paths required here ...
-			}
-		);
-
-
-		PrivateIncludePaths.AddRange(
-			new string[]
-			{
-				// ... add other private include paths required here ...
-			}
-		);
-
-
 		PublicDependencyModuleNames.AddRange(
 			new[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
 				"AnimGraphRuntime",
 				"GameplayTags"
 			}
 		);
-		
+
 		// StructUtils plugin has been integrated into the engine starting from 5.5
 		if (Target.Version.MajorVersion == 5 && Target.Version.MinorVersion <= 4)
 		{
@@ -47,18 +30,12 @@ public class KawaiiPhysics : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore"
-				// ... add private dependencies that you statically link with here ...	
 			}
 		);
 
-
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-		);
-
-		if (Target.bBuildEditor) PublicDependencyModuleNames.Add("UnrealEd");
+		if (Target.bBuildEditor)
+		{
+			PublicDependencyModuleNames.Add("UnrealEd");
+		}
 	}
 }
--- Plugins/KawaiiPhysics/Source/KawaiiPhysicsEd/KawaiiPhysicsEd.Build.cs
+++ Plugins/KawaiiPhysics/Source/KawaiiPhysicsEd/KawaiiPhysicsEd.Build.cs
@@ -8,29 +8,35 @@ public class KawaiiPhysicsEd : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore", "KawaiiPhysics" });
 		PrivateDependencyModuleNames.AddRange(new[]
 		{
-			"AnimGraph", "BlueprintGraph", "Persona", "UnrealEd", "AnimGraphRuntime", "Slate", "SlateCore",
-			"StructUtils"
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"KawaiiPhysics",
+			"AnimGraph",
+			"BlueprintGraph",
+			"Persona",
+			"UnrealEd",
+			"AnimGraphRuntime",
+			"Slate",
+			"SlateCore"
 		});
 
-		BuildVersion Version;
-		if (BuildVersion.TryRead(BuildVersion.GetDefaultFileName(), out Version))
-			if (Version.MajorVersion == 5)
+		if(Target.Version.MajorVersion >= 5)
+		{
+			PrivateDependencyModuleNames.Add("EditorFramework");
+			if (Target.Version.MinorVersion >= 1)
 			{
-				PrivateDependencyModuleNames.AddRange(new[] { "EditorFramework" });
-
-				// From UE5.1, BaseClass of EditMode move to new Module 
-				if (Version.MinorVersion >= 1) PrivateDependencyModuleNames.AddRange(new[] { "AnimationEditMode" });
+				PrivateDependencyModuleNames.Add("AnimationEditMode");
 			}
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+			// StructUtils plugin has been integrated into the engine starting from 5.5
+			if (Target.Version.MinorVersion <= 4)
+			{
+				PrivateDependencyModuleNames.Add("StructUtils");
+			}
+		}
 	}
 }
